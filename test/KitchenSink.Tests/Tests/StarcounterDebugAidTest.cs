bf9a3d4b003f49b20eedfa2ab47121de36080e18
ubbeK <urban.nyden@gmail.com>
2016-11-01 13:12:14 +0100

    Adding test for firefox using ExecuteScript for trigger the open and close functionality of starcounter-debug-aid, #73

---
+++ test/KitchenSink.Tests/Tests/StarcounterDebugAidTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using KitchenSink.Tests;
+using OpenQA.Selenium.Interactions;
+
+namespace KitchenSink.Tests.Tests
+{
+    [TestFixture("firefox")]
+    //[TestFixture("chrome")]
+    //[TestFixture("edge")]
+    //[TestFixture("internet explorer")]
+    public class StarcounterDebugAidTest : BaseTest
+    {
+        public StarcounterDebugAidTest(string browser) : base(browser)
+        {
+        }
+
+        [Test]
+        public void StarcounterDebugAidTest_OpenAndClose()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/Text");
+            this.WaitUntil(ExpectedConditions.PresenceOfAllElementsLocatedBy(ByHelper.StarcounterIncludeWithInputText));
+
+            var debugAidElement = driver.FindElement(By.ClassName("starcounter-debug-aid"));
+            Assert.IsFalse(debugAidElement.Displayed);
+
+            if (browser == "firefox")
+            {
+                OpenAndCloseWithJavaScript();
+            }
+            else
+            {
+                // Does not seem to work
+                OpenAndCloseWithActionsBuilder();
+            }
+        }
+
+        private void OpenAndCloseWithJavaScript()
+        {
+            (driver as OpenQA.Selenium.Remote.RemoteWebDriver).ExecuteScript(
+                string.Format("{0}{1}{2}",
+                "var keyEvent = document.createEvent('KeyboardEvent');",
+                "keyEvent.initKeyEvent(\"keydown\", true, true, null, true, false, false, false, 220, 0);",
+                "document.dispatchEvent(keyEvent);")
+            );
+
+            this.WaitUntil(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("html body starcounter-debug-aid")));
+            var debugAidElement = driver.FindElement(By.ClassName("starcounter-debug-aid"));
+            Assert.IsTrue(debugAidElement.Displayed);
+
+            (driver as OpenQA.Selenium.Remote.RemoteWebDriver).ExecuteScript(
+                string.Format("{0}{1}{2}",
+                "var keyEvent = document.createEvent('KeyboardEvent');",
+                "keyEvent.initKeyEvent(\"keydown\", true, true, null, false, false, false, false, 27, 0);",
+                "document.dispatchEvent(keyEvent);")
+            );
+
+            debugAidElement = driver.FindElement(By.ClassName("starcounter-debug-aid"));
+            Assert.IsFalse(debugAidElement.Displayed);
+        }
+
+        private void OpenAndCloseWithActionsBuilder()
+        {
+            new Actions(driver).KeyDown(Keys.Control).SendKeys("§").Build().Perform();
+
+            this.WaitUntil(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("html body starcounter-debug-aid")));
+            var debugAidElement = driver.FindElement(By.ClassName("starcounter-debug-aid"));
+            Assert.IsTrue(debugAidElement.Displayed);
+
+            new Actions(driver).SendKeys(Keys.Escape).Build().Perform();
+
+            debugAidElement = driver.FindElement(By.ClassName("starcounter-debug-aid"));
+            Assert.IsFalse(debugAidElement.Displayed);
+        }
+    }
+}
