4c6396f92fea5ab8d39641d511678ed5787a5c1e
Dimitris Thanasis <Dimitris.th@hotmail.com>
2016-11-17 14:26:18 +0100

    Everything works as intended now
    There is a list of people. Every person has a slot for "favorite game".
    When a user hovers each person, a loading will start (at least
    visually), and when it completes, it will have loaded the info (which
    their favorite game is)
    
    As this information is loaded, it is also filled into the rows.
    
    When a user starts loading data, and then leaves the object (with the
    mouse), that data needs to be re-loaded
    
    TODO: Write meaningful text, perhaps change data "retrieving" method to
    ScheduleTask, and perhaps simplify the code a bit

--- src/KitchenSink/LazyLoadingPage.json.cs
+++ src/KitchenSink/LazyLoadingPage.json.cs
@@ -26,13 +26,13 @@ namespace KitchenSink
 
         private void FillDummyData() // Just some random names, and added "a favorite game" just for fun
         {
-            CreatePerson(1, "Alicia", "Alcott", "The Last of Us");
-            CreatePerson(2, "Beatrice", "Black", "Dragon Age: Inquisition");
-            CreatePerson(3, "Claire", "Clancy", "Final Fantasy XIII");
-            CreatePerson(4, "Delilah", "Darcy", "World of Warcraft");
-            CreatePerson(5, "Ellie", "Earnhart", "Overwatch");
-            CreatePerson(6, "Faith", "Fahrlander", "Pokemon X");
-            CreatePerson(7, "Grace", "Gather", "Battlefield 4");
+            CreatePerson(1, "Alicia", "Alcott", "");
+            CreatePerson(2, "Beatrice", "Black", "");
+            CreatePerson(3, "Claire", "Clancy", "");
+            CreatePerson(4, "Delilah", "Darcy", "");
+            CreatePerson(5, "Ellie", "Earnhart", "");
+            CreatePerson(6, "Faith", "Fahrlander", "");
+            CreatePerson(7, "Grace", "Gather", "");
         }
 
         private void CreatePerson(int order, string firstName, string lastName, string favoriteGame) // function to create a person
@@ -58,6 +58,32 @@ namespace KitchenSink
 
             public void Handle(Input.IsHovered action) // TODO: NOT set DataIsLoaded to 1 if the user has left the area
             {
+                var test = this.FirstName;
+                var test2 = this.IsHovered;
+                var test3 = action;
+
+                //if (action.Value == 0) // On de-hover
+                //{
+                //    this.ParentPage.LoadVisualCss = "none";
+                //    this.ParentPage.DataVisualCss = "none";
+                //    return;
+                //}
+                //if (this.DataIsLoaded == 0)
+                //{
+                    //this.ParentPage.LoadVisualCss = "block";
+                    //this.ParentPage.DataVisualCss = "none";
+                //}
+                //else if (this.DataIsLoaded == 1)
+                //{
+                //    this.ParentPage.LoadVisualCss = "block";
+                //    this.ParentPage.DataVisualCss = "none";
+                //    return;
+                //}
+
+                CreateData();
+                //this.ParentPage.LoadVisualCss = "none"; // Disables the loading and shows the data after a while
+                //this.ParentPage.DataVisualCss = "block";
+
                 //if (action.Value == 0) // If the item gets de-hovered
                 //{
                 //    this.ParentPage.LoadVisualCss = "none"; // Displays the loading
@@ -65,35 +91,71 @@ namespace KitchenSink
                 //    return;
                 //}
 
-                if (this.DataIsLoaded == 1)
-                {
-                    this.DataToShow = this.FavoriteGame;
-                    this.ParentPage.DataVisualCss = "block"; // Changes the CSS of the data-box to "block" in order for it to appear on the screen
-                    return;
-                }
+                //if (this.DataIsLoaded == 1)
+                //{
+                //    this.DataToShow = this.FavoriteGame;
+                //    this.ParentPage.DataVisualCss = "block"; // Changes the CSS of the data-box to "block" in order for it to appear on the screen
+                //    return;
+                //}
 
                 //this.ParentPage.LoadVisualCss = "block"; // Displays the loading
                 //this.ParentPage.DataVisualCss = "none";
 
 
-                this.CreateData(); // Creates the data
-                if (action.Value == 1) // If this person is still being hovered - Need to get current value of isHovered
-                {
-                    this.DataIsLoaded = 1; // Functions like a bool. It sets it to true
-                }
+                //this.CreateData(); // Creates the data
+                //if (action.Value == 1) // If this person is still being hovered - Need to get current value of isHovered
+                //{
+                //    this.DataIsLoaded = 1; // Functions like a bool. It sets it to true
+                //}
 
-                this.ParentPage.LoadVisualCss = "none"; // Disables the loading and shows the data after a while
-                this.ParentPage.DataVisualCss = "block";
+                //this.ParentPage.LoadVisualCss = "none"; // Disables the loading and shows the data after a while
+                //this.ParentPage.DataVisualCss = "block";
 
-               // this.DataToShow = "Test"; // this.DataToShow = CreateDataAccordingly(); -> Return data
-                this.ParentPage.DisplayedData.DataContent = this.DataToShow;
+                // this.DataToShow = "Test"; // this.DataToShow = CreateDataAccordingly(); -> Return data
+                //this.ParentPage.DisplayedData.DataContent = this.DataToShow;
             }
 
-            public void CreateData ()
+            public void CreateData () // Maybe Switch to ScheduledTask?
             {
                 Thread.Sleep(1000);
-                this.DataToShow = this.FavoriteGame; // Temp data to fill - for UI test purposes
-                //switch case? compare the name and fill data accordingly?
+                FakeDataBase(this.FirstName);
+
+                this.ParentPage.DisplayedData.DataContent = this.DataToShow; // Updates the view model to display the correct data
+            }
+
+            public void FakeDataBase (string firstName) // Picks out different data depending on who is invoking this function
+            {
+                switch (firstName)
+                {
+                    case "Alicia":
+                        this.DataToShow = this.FavoriteGame = "The Last of Us";
+                        break;
+
+                    case "Beatrice":
+                        this.DataToShow = this.FavoriteGame = "Dragon Age: Inquisition";
+                        break;
+
+                    case "Claire":
+                        this.DataToShow = this.FavoriteGame = "Final Fantasy XIII";
+                        break;
+
+                    case "Delilah":
+                        this.DataToShow = this.FavoriteGame = "World of Warcraft";
+                        break;
+
+                    case "Ellie":
+                        this.DataToShow = this.FavoriteGame = "Overwatch";
+                        break;
+
+                    case "Faith":
+                        this.DataToShow = this.FavoriteGame = "Pokemon X";
+                        break;
+
+                    case "Grace":
+                        this.DataToShow = this.FavoriteGame = "Counter Strike";
+                        break;
+                }
+
             }
         }
 
