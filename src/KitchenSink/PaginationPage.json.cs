3d48a8fb9926d7ce533ea00dafb9b7d78b518d91
Erlend <elandroe@icloud.com>
2017-05-12 16:18:23 +0200

    refactor for readability

--- src/KitchenSink/PaginationPage.json.cs
+++ src/KitchenSink/PaginationPage.json.cs
@@ -1,4 +1,5 @@
 using Starcounter;
+using System.Linq;
 
 namespace KitchenSink
 {
@@ -16,143 +17,137 @@ namespace KitchenSink
         {
             base.OnData();
 
-            this.TotalEntries = Db.SQL<long>("SELECT COUNT(e) FROM KitchenSink.Book e").First;
-            InitPage();
+            GetNewPage();
+            SetTotalPages();
+            CreateNavButtons();
+            SetPageEntries();
         }
 
-        // Changes the number of entries per page depending on input and sets the page to 1
+        public long TotalEntries = Db.SQL<long>("SELECT COUNT(e) FROM KitchenSink.Book e").FirstOrDefault();
+
         void Handle(Input.EntriesPerPage action)
         {
-            this.CurrentOffset = 0;
-            this.EntriesPerPage = action.Value;
+            CurrentOffset = 0;
+            EntriesPerPage = action.Value;
             SetTotalPages();
             GetNewPage();
         }
 
-        // Sets the current page when the user clicks on one of the numbered buttons
         void Handle(Input.ChangePage action)
         {
-            this.CurrentOffset = this.EntriesPerPage * (action.Value - 1);
+            CurrentOffset = EntriesPerPage * (action.Value - 1);
             GetNewPage();
         }
 
-        // Goes to the next page on click
         void Handle(Input.NextPageTrigger action)
         {
-            if (this.CurrentOffset + this.EntriesPerPage < this.TotalEntries)
+            if (CurrentOffset + EntriesPerPage < TotalEntries)
             {
-                this.CurrentOffset = this.CurrentOffset + this.EntriesPerPage;
+                CurrentOffset = CurrentOffset + EntriesPerPage;
             }
             GetNewPage();
         }
 
-        // Goes to the previous page on click
         void Handle(Input.PreviousPageTrigger action)
         {
-            this.CurrentOffset = this.CurrentOffset - this.EntriesPerPage >= 0 ? this.CurrentOffset - this.EntriesPerPage : 0;
+            bool willOverflow = CurrentOffset - EntriesPerPage < 0;
+
+            CurrentOffset = willOverflow ? 0 : CurrentOffset - EntriesPerPage;
             GetNewPage();
         }
 
-        // Goes to the last page on click
         void Handle(Input.LastPageTrigger action)
         {
-            var remainder = this.TotalEntries % this.EntriesPerPage;
-            this.CurrentOffset = remainder != 0 ? this.TotalEntries - (remainder) : this.TotalEntries - this.EntriesPerPage;
+            long remainder = TotalEntries % EntriesPerPage;
+            if (remainder == 0)
+            {
+                CurrentOffset = TotalEntries - EntriesPerPage;
+            }
+            else
+            {
+                CurrentOffset = TotalEntries - remainder;
+            }
             GetNewPage();
         }
 
-        // Goes to the first page on click
         void Handle(Input.FirstPageTrigger action)
         {
-            this.CurrentOffset = 0;
+            CurrentOffset = 0;
             GetNewPage();
         }
 
-        // Initializes the page by calling all the neccesary methods to set the page to 1
-        private void InitPage()
-        {
-            GetNewPage();
-            SetTotalPages();
-            CreateNavButtons(this.EntriesPerPage);
-            SetPageEntries();
-        }
-
-        // Creates the choices for page entries.
-        // Modify the array entryChoices to modify how many entries per page the user can choose between.
         private void SetPageEntries()
         {
-            int[] entryChoices = new int[] { 5, 15, 30 };
-            foreach (int entry in entryChoices)
+            int[] entriesPerPageAlternatives = new int[] { 5, 15, 30 };
+            foreach (int entry in entriesPerPageAlternatives)
             {
-                var page = this.PageEntries.Add();
+                PageEntriesElementJson page = PageEntries.Add();
                 page.Amount = entry;
                 page.Text = $"Show {entry} items per page";
             }
         }
 
-        // Sets the JSON value for total pages    
         private void SetTotalPages()
         {
-            var totalPagesRounded = this.TotalEntries / this.EntriesPerPage;
-            this.TotalPages = this.TotalEntries % this.EntriesPerPage == 0 ? totalPagesRounded : totalPagesRounded + 1;
+            long totalPagesRounded = TotalEntries / EntriesPerPage;
+            TotalPages = TotalEntries % EntriesPerPage == 0 ? totalPagesRounded : totalPagesRounded + 1;
         }
 
         // Establishes the navigation buttons.
         // Displays all the page buttons if there are less then 10,
         // Otherwise it shows the current page, the two before and after.
-        private void CreateNavButtons(long entriesPerPage)
+        private void CreateNavButtons()
         {
-            this.Pages.Clear();
-            this.CurrentPage = this.CurrentOffset / entriesPerPage + 1;
+            Pages.Clear();
+            CurrentPage = CurrentOffset / EntriesPerPage + 1;
 
-            if (this.TotalPages < 10)
+            if (TotalPages < 10)
             {
-                for (long i = 1; i < this.TotalPages + 1; i++)
+                for (long i = 1; i < TotalPages + 1; i++)
                 {
                     CreateButton(i);
                 }
             }
 
-            else if (this.TotalPages >= 10)
+            else if (TotalPages >= 10)
             {
                 long pagesBefore = -2;
                 long pagesAfter = 3;
 
-                if (this.CurrentPage + pagesAfter > this.TotalPages)
+                if (CurrentPage + pagesAfter > TotalPages)
                 {
-                    pagesBefore -= (this.CurrentPage + pagesAfter - 1) % this.TotalPages;
+                    pagesBefore -= (CurrentPage + pagesAfter - 1) % TotalPages;
                 }
 
-                if (this.CurrentPage + pagesBefore <= 0)
+                if (CurrentPage + pagesBefore <= 0)
                 {
-                    pagesAfter -= (this.CurrentPage + pagesBefore - 1);
+                    pagesAfter -= (CurrentPage + pagesBefore - 1);
                 }
 
-                for (long i = this.CurrentPage + pagesBefore; i < this.CurrentPage + pagesAfter; i++)
+                for (long i = CurrentPage + pagesBefore; i < CurrentPage + pagesAfter; i++)
                 {
-                    if (i > 0 && i < this.TotalPages + 1)
+                    if (i > 0 && i < TotalPages + 1)
                     {
                         CreateButton(i);
                     }
                 }
             }
-            this.DisableLast = this.CurrentPage == this.TotalPages;
-            this.DisableFirst = this.CurrentPage == 1;
+            DisableLast = CurrentPage == TotalPages;
+            DisableFirst = CurrentPage == 1;
         }
 
         // Creates a nav button by setting the JSON to the current page number and if it's active.
         private void CreateButton(long pageNumber)
         {
-            var page = this.Pages.Add();
+            PagesElementJson page = Pages.Add();
             page.PageNumber = pageNumber;
-            page.Active = (this.CurrentPage == pageNumber);
+            page.Active = (CurrentPage == pageNumber);
         }
 
-        // Switch to the desired page depeding on the current offset.
         private void GetNewPage()
         {
-            this.Library.Data = Db.SQL<Book>("SELECT b FROM KitchenSink.Book b ORDER BY b.Position FETCH ? OFFSET ?", this.EntriesPerPage, this.CurrentOffset);
-            CreateNavButtons(this.EntriesPerPage);
+            Library.Data = Db.SQL<Book>("SELECT b FROM KitchenSink.Book b ORDER BY b.Position FETCH ? OFFSET ?", EntriesPerPage, CurrentOffset);
+            CreateNavButtons();
         }
     }
 }
