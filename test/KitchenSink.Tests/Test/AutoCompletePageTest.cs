dd946a1d7d0f57daaea9d44dbdb043ec3d9dd5b3
Patryk Szwermer <patryk.szwer@gmail.com>
2017-10-05 15:57:54 +0200

    Removed Parallelizable attribute for test purposes

--- test/KitchenSink.Tests/Test/AutoCompletePageTest.cs
+++ test/KitchenSink.Tests/Test/AutoCompletePageTest.cs
@@ -4,7 +4,6 @@ using NUnit.Framework;
 
 namespace KitchenSink.Tests.Test
 {
-    [Parallelizable(ParallelScope.Fixtures)]
     [TestFixture(Config.Browser.Chrome)]
     [TestFixture(Config.Browser.Edge)]
     [TestFixture(Config.Browser.Firefox)]
